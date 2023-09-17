364837d1b6be6b780e88980519dded4eb699f291
“Jhonny <aniceto.0760@gmail.com>
2023-09-17 17:26:05 -0400

    Practica Dashboard y login listo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth/auth.component';
+import { PagesComponent } from './pages/pages.component';
+
+const routes: Routes = [
+
+{
+  path: '',
+  redirectTo: '/login',
+  pathMatch: 'full'
+},
+
+{
+  path: 'login',
+  component: AuthComponent
+},
+
+{
+  path: 'dashboard',
+  component: PagesComponent,
+  children: [
+
+    {
+      path: 'student',
+      loadChildren: ()=> import('./student/student.module').then(m => m.StudentModule)
+    }
+  ]
+},
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
